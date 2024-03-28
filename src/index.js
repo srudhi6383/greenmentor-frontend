5a99abf9e2c6630b0abcce24d1a397b8c42220c1
srudhi6383 <srudhi24@gmail.com>
2024-03-28 14:16:28 +0530

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import store from './Redux/store';
+import store from './store';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from 'redux';
 import {thunk} from 'redux-thunk';
-import { loginReducer } from './Login/reducer';
-import { signupReducer } from './Register/reducer';
-import { taskReducer} from './Task/reducer'
+import { loginReducer } from './Redux/Login/reducer';
+import { signupReducer } from './Redux/Register/reducer';
+import { taskReducer} from './Redux/Task/reducer'
 
 const rootReducer = combineReducers({loginReducer,signupReducer,taskReducer});
 
